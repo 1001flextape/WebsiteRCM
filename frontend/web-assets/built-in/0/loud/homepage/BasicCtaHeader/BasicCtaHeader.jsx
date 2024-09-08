702918dd371269c6398a52ab495a0b394c5888c6
Michael Hunt <1001flextape@gmail.com>
2024-09-08 12:41:48 -0700

    Loud Section Basic Call To Action completed.

--- frontend/web-assets/built-in/0/loud/homepage/BasicCtaHeader/BasicCtaHeader.jsx
+++ frontend/web-assets/built-in/0/loud/homepage/BasicCtaHeader/BasicCtaHeader.jsx
@@ -1,29 +1,106 @@
 import React from 'react';
 import styles from './BasicCtaHeader.module.css';
 
-const MainVideoHeader = (props) => {
-  const { system } = props.data;
-  const { isNightMode } = system.state;
+const getTextColor = (suggestedTextColor) => {
+  console.log('suggestedTextColor', suggestedTextColor)
+  switch (suggestedTextColor) {
+    case "LIGHT":
+      return "#f1f4f5"
+    case "DARK":
+      return "rgb(96, 96, 96)"
+    default:
+      return "#f1f4f5"
+  }
+}
+
+const BasicCtaHeader = (props) => {
+  const { system, user } = props.data;
+  const {
+    isDayMode,
+    isNightMode,
+    isDisplayMode,
+    isFunctionalMode,
+    isProdMode,
+    assetApiUrl,
+  } = system.state;
+  const {
+    navigate
+  } = system.utils
+
+  let isShowing = true;
+  let header = "Empower Your Small Business"
+  let description = "Unlock success with tailored solutions and expert guidance."
+  let buttonText = "Get Started"
+
+  let backgroundStyles = {}
+  let buttonStyles = {}
+
+  if (isFunctionalMode) {
+    isShowing = user?.isShowing?.value || true;
+    header = user?.header?.value
+    description = user?.description?.value
+    buttonText = user?.ctaButtonText?.value
+
+    backgroundStyles = {
+      backgroundColor: isNightMode
+        ? user?.backgroundColorNight?.value?.color
+        : user?.backgroundColorDay?.value?.color,
+      color: isNightMode
+        ? getTextColor(user?.backgroundColorNight?.value?.suggestedTextColor)
+        : getTextColor(user?.backgroundColorDay?.value?.suggestedTextColor),
+    }
+
+    buttonStyles = {
+      backgroundColor: isNightMode
+        ? user?.ctaButtonColorNight?.value?.color
+        : user?.ctaButtonColorDay?.value?.color,
+      color: isNightMode
+        ? getTextColor(user?.ctaButtonColorNight?.value?.suggestedTextColor)
+        : getTextColor(user?.ctaButtonColorDay?.value?.suggestedTextColor),
+    }
+
+  }
+
+  // backgroundColorDay: Object { type: "COLOR_SELECTION:V1", value: {…} }
+  // backgroundColorNight: Object { type: "COLOR_SELECTION:V1", value: {…} }
+  // ctaButtonColorDay: Object { type: "COLOR_SELECTION:V1", value: {…} }
+  // ctaButtonColorNight: Object { type: "COLOR_SELECTION:V1", value: {…} }
+  // ctaButtonLink: Object { type: "LINK_SELECTION:V1", value: "/p/test/neat_test" }
+  // ctaButtonText: Object { type: "YDOC:V1", value: "blah blah blah" }
+  // description: Object { type: "WYSIWYG:V1", value: {…} }
+  // header: Object { type: "YDOC:V1", value: "blah" }
+  // isShowing: Object { type: "SWITCH:V1", value: true }
 
   return (
-    <section className={`${styles.section} ${isNightMode ? styles.sectionNight : ''}`}>
-      <div className={`${styles.overlay} ${isNightMode ? styles.overlayNight : ''}`}></div>
-      <div className={styles.content}>
-        <h1 className={`${styles.heading} ${isNightMode ? styles.headingNight : ''}`}>
-          Empower Your Small Business
-        </h1>
-        <p className={`${styles.subheading} ${isNightMode ? styles.subheadingNight : ''}`}>
-          Unlock success with tailored solutions and expert guidance.
-        </p>
-        <a
-          href="#cta" // Replace with your desired link
-          className={`${styles.ctaButton} ${isNightMode ? styles.ctaButtonNight : ''}`}
+    <>
+      {isShowing && (
+        <section
+          className={`${styles.section} ${isNightMode ? styles.sectionNight : ''}`}
+          style={backgroundStyles}
         >
-          Get Started
-        </a>
-      </div>
-    </section>
+          <div className={`${styles.overlay} ${isNightMode ? styles.overlayNight : ''}`}></div>
+          <div className={styles.content}>
+            <h1 className={`${styles.heading} ${isNightMode ? styles.headingNight : ''}`}>
+              {header}
+            </h1>
+            <p className={`${styles.subheading} ${isNightMode ? styles.subheadingNight : ''}`}
+              dangerouslySetInnerHTML={{ __html: description }}
+            >
+
+            </p>
+            <a
+              href="#cta" // Replace with your desired link
+              className={`${styles.ctaButton} ${isNightMode ? styles.ctaButtonNight : ''}`}
+              style={buttonStyles}
+              onClick={isProdMode ? navigate(user.ctaButtonLink.value) : () => { }}
+            >
+              {buttonText}
+            </a>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
-export default MainVideoHeader;
+export default BasicCtaHeader;
