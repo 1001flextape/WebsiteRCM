999a1bf4e64ee8df436aaeefa8bc2b87469ad735
Michael Hunt <1001flextape@gmail.com>
2024-09-08 20:20:49 -0700

    Youtube video component completed.

--- frontend/web-assets/built-in/0/loud/homepage/YoutubeVideoHeader/YoutubeVideoHeader.jsx
+++ frontend/web-assets/built-in/0/loud/homepage/YoutubeVideoHeader/YoutubeVideoHeader.jsx
@@ -3,9 +3,68 @@ import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
 import styles from './YoutubeVideoHeader.module.css';
 
+const getTextColor = (suggestedTextColor) => {
+  switch (suggestedTextColor) {
+    case "LIGHT":
+      return "#f1f4f5"
+    case "DARK":
+      return "rgb(96, 96, 96)"
+    default:
+      return "#f1f4f5"
+  }
+}
+
 const YoutubeVideoHeader = (props) => {
-  const { system } = props.data;
-  const { isNightMode } = system.state;
+  const { system, user } = props.data;
+  const {
+    isDayMode,
+    isNightMode,
+    isDisplayMode,
+    isFunctionalMode,
+    isProdMode,
+    assetApiUrl,
+  } = system.state;
+  const {
+    navigate
+  } = system.utils
+
+  let isShowing = true
+  let videoId = "PBQZe3Y-pi4"
+  let header = "Empower Your Small Business"
+  let description = "Unlock success with tailored solutions and expert guidance."
+  let buttonText = "Get Started"
+  let buttonLink;
+
+  // styles
+  let buttonStyles = {}
+  let watchStyles = {}
+
+  // init
+  if (isFunctionalMode) {
+    isShowing = user?.isShowing?.value || true
+    videoId = user?.youtubeVideoId?.value
+    header = user?.header?.value
+    description = user?.description?.value
+    buttonText = user?.ctaButtonText?.value
+    buttonLink = user?.ctaButtonLink?.value
+
+    buttonStyles = {
+      backgroundColor: isNightMode
+        ? user?.ctaButtonColorNight?.value?.color
+        : user?.ctaButtonColorDay?.value?.color,
+      color: isNightMode
+        ? getTextColor(user?.ctaButtonColorNight?.value?.suggestedTextColor)
+        : getTextColor(user?.ctaButtonColorDay?.value?.suggestedTextColor),
+    }
+
+    watchStyles = {
+      color: isNightMode
+        ? user?.ctaButtonColorNight?.value?.color
+        : user?.ctaButtonColorDay?.value?.color,
+    }
+  }
+
+
 
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -18,43 +77,69 @@ const YoutubeVideoHeader = (props) => {
   };
 
   return (
-    <section className={`${styles.section} ${isNightMode ? styles.sectionNight : ''}`}>
-      <iframe
-        title="YouTube Video"
-        width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/PBQZe3Y-pi4?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=1&modestbranding=1&rel=0&playlist=PBQZe3Y-pi4"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        allowFullScreen
-        className={styles.video}
-      ></iframe>
+    <>
+      {isShowing && (
+        <section className={`${styles.section} ${isNightMode ? styles.sectionNight : ''}`}>
+          {videoId && (
+            <iframe
+              title="YouTube Video"
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&loop=1&modestbranding=1&rel=0&playlist=PBQZe3Y-pi4`}
+              frameBorder="0"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+              className={styles.video}
+            ></iframe>
+          )}
 
-      <div className={styles.overlay}></div>
+          <div className={styles.overlay}></div>
 
-      <div className={`${styles.content} ${isNightMode ? styles.contentNight : ''}`}>
-        <h1 className={styles.heading}>Empower Your Small Business</h1>
-        <p className={styles.subheading}>Unlock success with tailored solutions and expert guidance.</p>
-        <div className={styles.buttons}>
-          <a href="#cta" className={styles.ctaButton}>Get Started</a>
-          <Button onClick={handleOpenModal} className={styles.watchButton}>Watch Video</Button>
-        </div>
-      </div>
+          <div className={`${styles.content} ${isNightMode ? styles.contentNight : ''}`}>
+            <h1 className={styles.heading}>{header}</h1>
+            <p
+              dangerouslySetInnerHTML={{ __html: description }}
+              style={{ paddingBottom: "5px" }}
+            ></p>
+            <div className={styles.buttons}>
+              <a href="#cta"
+                className={styles.ctaButton}
+                style={buttonStyles}
+                onClick={isProdMode
+                  ? navigate(buttonLink)
+                  : () => { }
+                }
+              >
+                {buttonText}
+              </a>
+              <Button
+                onClick={handleOpenModal}
+                className={styles.watchButton}
+                style={watchStyles}
+              >
+                Watch Video
+              </Button>
+            </div>
+          </div>
 
-      <Modal open={modalOpen} onClose={handleCloseModal}>
-        <div className={styles.modalContainer}>
-          <iframe
-            title="YouTube Video"
-            width="100%"
-            height="100%"
-            src="https://www.youtube.com/embed/PBQZe3Y-pi4?autoplay=1&controls=0&showinfo=0&modestbranding=1&rel=0"
-            frameBorder="0"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-          ></iframe>
-        </div>
-      </Modal>
-    </section>
+          <Modal open={modalOpen} onClose={handleCloseModal}>
+            <div className={styles.modalContainer}>
+              {videoId && (
+                <iframe
+                  title="YouTube Video"
+                  width="100%"
+                  height="100%"
+                  src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&showinfo=0&modestbranding=1&rel=0`}
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                ></iframe>
+              )}
+            </div>
+          </Modal>
+        </section>
+      )}
+    </>
   );
 };
 
