7c15de4a436bb52f2feac8bb6a0735227f8987c1
Michael Hunt <1001flextape@gmail.com>
2024-09-10 17:23:36 -0700

    Top Three Highlight Completed.

--- frontend/web-assets/built-in/0/sections/TopThreeHighlights/TopThreeHighlights.jsx
+++ frontend/web-assets/built-in/0/sections/TopThreeHighlights/TopThreeHighlights.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import styles from './TopThreeHighlights.module.css';
 
+const getTextColor = (suggestedTextColor) => {
+  switch (suggestedTextColor) {
+    case "LIGHT":
+      return "#f1f4f5"
+    case "DARK":
+      return "rgb(96, 96, 96)"
+    default:
+      return "#f1f4f5"
+  }
+}
+
 const TopThreeHighlights = (props) => {
-  const { system } = props.data;
-  const { isNightMode } = system.state;
+
+  const { system, user } = props.data;
+  const {
+    isDayMode,
+    isNightMode,
+    isDisplayMode,
+    isFunctionalMode,
+    isProdMode,
+    assetApiUrl,
+  } = system.state;
+  const {
+    navigate
+  } = system.utils
+
+  let isShowing = true
+  let header = "Why Choose Us?"
+  let description = "  Explore the key reasons to choose us. From exceptional service to swift delivery and dedicated customer satisfaction, we're committed to exceeding your expectations.";
 
   const highlights = [
     {
@@ -23,13 +49,36 @@ const TopThreeHighlights = (props) => {
     },
   ];
 
+
+  let backgroundStyle = {}
+
+  if (isFunctionalMode) {
+    isShowing = user?.isShowing?.value || true
+    header = user?.header?.value
+    description = user?.description?.value
+
+    backgroundStyle = {
+      backgroundColor: isNightMode
+        ? user?.backgroundColorNight?.value?.color
+        : user?.backgroundColorDay?.value?.color,
+      color: isNightMode
+        ? getTextColor(user?.backgroundColorNight?.value?.suggestedTextColor)
+        : getTextColor(user?.backgroundColorDay?.value?.suggestedTextColor),
+    }
+  }
+
   return (
-    <section className={isNightMode ? styles.sectionNight : styles.section}>
+    <section
+      className={isNightMode ? styles.sectionNight : styles.section}
+      style={backgroundStyle}
+    >
       <div className={styles.container}>
-        <h3 className={isNightMode ? styles.headingNight : styles.heading}>Why Choose Us?</h3>
-        <p className={isNightMode ? styles.descriptionNight : styles.description}>
-          Explore the key reasons to choose us. From exceptional service to swift delivery and dedicated customer satisfaction, we're committed to exceeding your expectations.
-        </p>
+        <h3 className={isNightMode ? styles.headingNight : styles.heading}>{header}</h3>
+        <div
+          className={isNightMode ? styles.descriptionNight : styles.description}
+          dangerouslySetInnerHTML={{ __html: description }}
+        >
+        </div>
 
         <div className={styles.highlights}>
           {highlights.map((highlight, index) => (
